e84b0d93552e0539a305acc2cb142cd5e20e069a
Joary13 <jolea13008@gmail.com>
2023-11-05 18:09:49 +0100

    solution rectifiée

--- parenthese.js
+++ parenthese.js
@@ -1,45 +1,31 @@
 // trouver la longeur (integer) de la plus longue série "normale" de parenthèses
 
-const data = ')((())))(()())';
+// const data = ')((())))(()())';
+const data = ')()(()))';
 
-const tab = data.split('');
-
-let max = 0;
-
-/**
- * valide que le symbole est bien symétrique selon l'axe de symétrie qui correspond à l'index
- * @param {integer} ind1
- * @param {integer} ind2
- * @returns
- */
-
-const validation = (ind1, ind2) => {
-  if (
-    (tab[ind1] === '(' && tab[ind2] === ')') ||
-    (tab[ind1] === ')' && tab[ind2] === '(')
-  ) {
-    return true;
-  } else {
-    return false;
+const maxBracketLength = (data) => {
+  const tab = data.split('');
+  let max = 0;
+  for (let i = 0; i < tab.length; i++) {
+    let unclosedBracket = 0;
+    let sequenceBracket = 0;
+    for (let j = i; j < tab.length; j++) {
+      if (tab[j] === '(') {
+        unclosedBracket++;
+      }
+      if (tab[j] === ')') {
+        unclosedBracket--;
+      }
+      if (unclosedBracket < 0) {
+        break;
+      }
+      if (unclosedBracket === 0) {
+        sequenceBracket = j - i + 1;
+      }
+    }
+    if (sequenceBracket > max) max = sequenceBracket;
   }
+  console.log(`la longueur max de parenthèses est: ${max}`);
 };
 
-for (let i = 0; i < tab.length - 1; i++) {
-  let j = 0;
-  let openedBracket = 0;
-  let closedBracket = 0;
-  while (j <= i && validation(i - j, i + 1 + j)) {
-    if (tab[i - j] === '(') {
-      openedBracket++;
-    } else if (tab[i - j] === ')') {
-      closedBracket++;
-    }
-    //if (openedBracket === 1 && max === 0) max = 2;
-    if (tab[i - j] === '(' && openedBracket >= closedBracket) {
-      if (max < 2 * (j + 1)) max = 2 * (j + 1);
-    }
-    j++;
-  }
-}
-
-console.log(`la longueur max est de ${max}`);
+maxBracketLength(data);
